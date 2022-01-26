2acf67199105bffe4fe6b598449c7b0dd66b913d
Hitesh Lalwani <lwni@protonmail.com>
2022-01-26 16:54:49 +0530

    Added site to github pages

--- scripts/button.js
+++ scripts/button.js
@@ -1,7 +1,7 @@
 let gs_btn = document.getElementById("get-started-btn-flx-cntr");
 if (gs_btn != null) {
   gs_btn.addEventListener("click", () => {
-    window.location.href = "/features.html";
+    window.location.href = "./features.html";
   });
 }
 
@@ -9,14 +9,14 @@ document.getElementById("feedback-btn").addEventListener("click", (e) => {
   window.location.href = "https://forms.gle/gHay1B669W3KPcBf7";
 });
 document.getElementById("logo").addEventListener("click", (e) => {
-  window.location.href = "/index.html";
+  window.location.href = "./index.html";
 });
 let features_cards = document.getElementsByClassName("features-card");
 let urls = [
-  "/note.html",
-  "/blockchain.html",
-  "/joke.html",
-  "/do_they_like_you.html",
+  "./note.html",
+  "./blockchain.html",
+  "./joke.html",
+  "./do_they_like_you.html",
   "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 ];
 
